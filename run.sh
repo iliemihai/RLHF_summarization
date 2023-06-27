3a21788050a48ce24ad39ea3c073c07be534a258
iliemihai92 <mihai.ilie@morningstar.com>
2023-06-27 14:42:22 +0000

    eleventh

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Split data
-#python3.8 preprocess/preprocess_data.py
+python3.8 preprocess/preprocess_data.py
 
 
 # Finetune with Supervised learning
-#cd project/sft/
-#python3.8 train.py
+cd project/sft/
+python3.8 train.py
 
 cd project/sft/
 cd ../rw
 # create Reward model dataset
-#python3.8 generate_rejected_summaries.py
+python3.8 generate_rejected_summaries.py
 # train reward model
-#python3.8 train.py #--inference
+python3.8 train.py #--inference
 
 cd ../rl
 # create Reinforcement Learning model dataset
-#python3.8 generate_rl_dataset.py
+python3.8 generate_rl_dataset.py
 # finetune with RLHF
 python3.8 train.py --inference
